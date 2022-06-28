f7f291865364d7ac4a45b8c96552a4f0efe15511
Bruno <cingolanib@gmail.com>
2022-06-28 16:04:25 -0300

    comienzo integracion con backend

---
+++ src/controllers/webserviceController.js
@@ -0,0 +1,32 @@
+const urlApi = "http://localhost:4000/";
+//const urlApi = "https://api-viernes.herokuapp.com/";
+console.log("url",urlApi);
+
+const urlWebServices = {
+
+    //users
+    loginUser:urlApi +"api/users/login",
+    createUser:urlApi +"api/users/",    
+    getUserByToken:urlApi +"api/users/me",
+    getUserByEmail:urlApi +"api/users/email",
+    updateUser:urlApi +"api/users/update",
+
+
+
+    //imagenes (template)
+    uploadFileImg: urlApi + "utils/uploadImg",
+    guardarImgUser: urlApi + "api/users/guardarImgUser",
+    getImgUser: urlApi + "api/users/imgUserByMail",
+    uploadFileImg: urlApi + "api/users/uploadImg",
+
+
+    //recetas
+    obtenerRecetas:urlApi +"api/recetas/",
+    crearReceta:urlApi +"api/recetas/crear-receta/",
+    editarReceta:urlApi +"api/recetas/editar-receta/",
+    eliminarReceta:urlApi +"api/recetas/eliminar-receta/",
+    buscarReceta:urlApi +"api/recetas/buscar/",
+    calificarReceta:urlApi +"api/recetas/calificar-receta/"
+}
+
+export default urlWebServices;
